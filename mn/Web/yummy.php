c83cb221cb1b0110fd0c0388b51b075816c10cde
rtklohia <73308653+rtklohia@users.noreply.github.com>
2020-10-22 23:43:55 +0530

    Add files via upload

---
+++ mn/Web/yummy.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+NAME: <?php echo $_GET["name"]; ?><br>
+  YEAR:  <?php echo $_GET["year"];?><br>
+  BRANCH:  <?php echo $_GET["dropdown"]; ?><br>
+  SKILLS:  <?php echo $_GET["skills"];?><br>
+  EMAIL ADRESS:  <?php echo $_GET["email"];?><br>
+  
+
+</body>
+</html>
